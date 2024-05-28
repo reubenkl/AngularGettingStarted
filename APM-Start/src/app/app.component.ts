0dd6207e7478d725b6ba0dd876bf9b83765f124e
reuben_kl <reubenkl1602@gmail.com>
2024-05-28 15:25:36 +0530

    Adding Service layer and HttpClientModule and calling API from service layer

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
   styleUrls : ['./app.component.css']
 })
 export class AppComponent{
-  pagetitle : string = "Angular sample project"
+  pagetitle : string = "Product List Management"
 };
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpaces } from './shared/convert-to-space';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpaces,
+    StarComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   bootstrap: [AppComponent]
 })
